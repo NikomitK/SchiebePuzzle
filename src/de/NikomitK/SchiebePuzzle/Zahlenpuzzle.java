b924c40c2ea442b8d68961486ac8b4510fc90d62
NikomitK <nikobo2003@gmail.com>
2021-03-18 11:19:28 +0100

    dings

--- src/de/NikomitK/SchiebePuzzle/Zahlenpuzzle.java
+++ src/de/NikomitK/SchiebePuzzle/Zahlenpuzzle.java
@@ -2,9 +2,11 @@ package de.NikomitK.SchiebePuzzle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Zahlenpuzzle extends Schiebepuzzle {
 
+    private Random r;
     private int [] aZahlenFeld;
     private List<Integer> aUnusedNums;
     public Zahlenpuzzle(int pGroesse){
@@ -25,12 +27,38 @@ public class Zahlenpuzzle extends Schiebepuzzle {
     }
 
     private void erzeugePuzzleFeld(){
+        r = new Random();
         System.out.println("erzeugePuzzleFeld");
 
         int groesse = gibGroesse();
 
         aZahlenFeld = new int[groesse * groesse];
-        aUnusedNums = new ArrayList<>(); //idk ob das passt, selbstversuch
+
+        for(int i = 0; i < aZahlenFeld.length; i++){
+            aZahlenFeld[i] = i;
+            System.out.println(aZahlenFeld[i]);
+        }
+        // absolut kein plan ob das so passt, erstmal nicht abschreiben
+        for(int i = 0; i<aZahlenFeld.length; i++){
+            int zufallsPosition = r.nextInt(8);
+            int temp = aZahlenFeld[i];
+            aZahlenFeld[i] = aZahlenFeld[zufallsPosition];
+            aZahlenFeld[zufallsPosition] = temp;
+        }
+
+        System.out.println();
+        for (int i = 0; i < aZahlenFeld.length; i++) System.out.println(aZahlenFeld[i]);
+//        eigene methode für zufall
+//        aUnusedNums = new ArrayList<>(); //idk ob das passt, selbstversuch
+//        for(int i = 0; i<groesse*groesse; i++){
+//            aUnusedNums.add(i);
+//        }
+//        for(int i = 0; i<aZahlenFeld.length; i++){
+//            int temp = r.nextInt(aUnusedNums.size());
+//            aZahlenFeld[i] = aUnusedNums.get(temp);
+//            aUnusedNums.remove(temp);
+//        System.out.println(aZahlenFeld[i]);
+//        }
     }
 
     public void tauscheTastenWerte(int pIdxTaste, int pIdxLeertaste){
